22bde230eec82a7dda49c191b77ba9dade6e5172
F.AlSaleh <94alsaleh@gmail.com>
2021-12-26 23:00:58 +0300

    Done

--- loops.js
+++ loops.js
@@ -10,16 +10,29 @@
  */
 function drawVLine(n) {
   // Continue the code here
+  for (let index = 0; index <n; index++) {
+  
+    console.log(`#`)
+  }
 }
+//drawVLine(5);
 /**
  *
  * In this function, you will have to draw a horizontal line with `n` length
  * for example `drawHLine(5) should print out # # # # # (there are spaces here!)
  */
-function drawHLine(n) {
-  // Continue the code here
+function print (text){
+  process.stdout.write(text);
 }
 
+function drawHLine(n) {
+  // Continue the code here
+  for (let i = 0; i < n; i++) {
+    print("# ")
+  }
+  console.log();
+  }
+  //drawHLine (5);
 /**
  * 
  * In this function, you will have to draw a square with a length of `n` 
@@ -34,7 +47,16 @@ function drawHLine(n) {
  */
 function drawSquare(n) {
   // Continue the code here
+  for (let r = 0; r < n; r++) {
+    
+    for (let  c= 0; c<n; c++) {
+      print ("# ")
+    }
+    console.log ();
+  }
 }
+//drawSquare(5);
+
 
 /**
  * 
@@ -50,8 +72,14 @@ function drawSquare(n) {
  */
 function drawLeftTriangle(n) {
   // Continue the code here
+  for (let r = 0; r < n; r++) {
+    for (let c = 0; c <= r; c++) {
+     print ("# ")
+    }
+    console.log()
+  }
 }
-
+drawLeftTriangle (5)
 /**
  * 
  * In this function, you will have to draw a tree with a length of `n` 
@@ -66,7 +94,21 @@ function drawLeftTriangle(n) {
  */
 function drawTree(n) {
   // Continue the code here
+  for (let r = 0; r < n; r++) {
+    for (let s = n; s >=1; s--) {
+      print (" ")
+    }
+      for (let c = 0; c <= r; c++) {
+        print("# ")
+      }
+    console.log();
+  }
+  for (let i = 0; i < n; i++) {
+    print(" ")
+  }
+  console.log("#")
 }
+drawTree (10);
 /**
  *
  * In this example, you will create 4 arrays, `yellowCards`, `greenCards`, `redCards`, `blueCards`
@@ -78,4 +120,12 @@ function fillUnoDeck() {
   let redCards = [];
   let blueCards = [];
   // Fill the cards here
+  for (let i = 0; i < 10; i++) {
+    greenCards.push(i);
+    yellowCards.push(i);
+    redCards.push(i);
+    blueCards.push(i);
+  }
+  console.log(greenCards, yellowCards, redCards, blueCards);
 }
+fillUnoDeck();
